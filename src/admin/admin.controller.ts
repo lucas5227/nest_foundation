eb96ea1c987f23e84f09c62f5526ca905dac91de
lucas <klucas5227@gmail.com>
2024-10-13 16:47:42 +0900

    DAY.10 admin board write list view ing

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -10,11 +10,10 @@ export class AdminController {
   constructor(
     private readonly memberService: MemberService,
     private readonly PostService: PostService,
-  ) {
-  }
+  ) {}
 
   @Get('')
-  @Render('admin/layout/layout') // index.ejs 템플릿을 렌더링
+  @Render('_admin/_layout/layout') // index.ejs 템플릿을 렌더링
   async main() {
     // TODO: 비로그인시 admin 접근 로그인
     // TODO: 관리자 아닐때 user 메인으로
@@ -22,55 +21,66 @@ export class AdminController {
   }
 
   @Get('memberList')
-  @Render('admin/layout/layout') // index.ejs 템플릿을 렌더링
+  @Render('_admin/_layout/layout') // index.ejs 템플릿을 렌더링
   async memberList() {
     // TODO: 비로그인시 admin 접근 로그인
     // TODO: 관리자 아닐때 user 메인으로
     const allMember = await this.memberService.getAllMember();
 
-    return { page: 'memberList.ejs', title: '회원관리', members: allMember };
+    return { page: 'member/list.ejs', title: '회원관리', members: allMember };
   }
 
   @Get('member/:id')
-  @Render('admin/layout/layout') // index.ejs 템플릿을 렌더링
+  @Render('_admin/_layout/layout') // index.ejs 템플릿을 렌더링
   async memberView(@Param('id') mem_id: number) {
     // TODO: 비로그인시 admin 접근 로그인
     // TODO: 관리자 아닐때 user 메인으로
     const member = await this.memberService.getOneMember(mem_id);
-    return { page: 'memberWrite.ejs', title: '회원정보수정', member: member };
+    return { page: 'member/write.ejs', title: '회원정보수정', member: member };
   }
 
   @Get('board/write/:brd_key')
-  @Render('admin/layout/layout') // index.ejs 템플릿을 렌더x`링
+  @Render('_admin/_layout/layout') // index.ejs 템플릿을 렌더x`링
   async postWrite() {
     // TODO: 비로그인시 admin 접근 로그인
     // TODO: 관리자 아닐때 user 메인으로
     const post = await this.memberService.getAllMember();
 
-    return { page: 'boardWrite.ejs', title: 'boardTitle', data: post };
+    return { page: 'board/write.ejs', title: 'boardTitle', data: post };
   }
 
   @Post('/board/write/:brd_key')
   @Redirect('/admin/board/brd_key')
-  async createPost(@Body() createPost: Partial<PostEntity>, @Req() req: Request): Promise<number> {
+  async createPost(
+    @Body() createPost: Partial<PostEntity>,
+    @Req() req: Request,
+  ): Promise<number> {
     createPost.post_register_ip = req.ip;
     return this.PostService.createPost(createPost);
   }
 
   @Get('board/:brd_key')
-  @Render('admin/layout/layout')
-  async list(@Param('brd_key') brd_key: string, @Query('cat_id') cat_id: string, @Query('page') page: number) {
+  @Render('_admin/_layout/layout')
+  async list(
+    @Param('brd_key') brd_key: string,
+    @Query('cat_id') cat_id: string,
+    @Query('page') page: number,
+  ) {
     // TODO: 비로그인시 admin 접근 로그인
     // TODO: 관리자 아닐때 user 메인으로
-    console.log('Board Key:', brd_key);
-    console.log('Category ID:', cat_id);
-
     const limit = 10; // 페이지당 항목 수
     const offset = (page - 1) * limit; // 현재 페이지에 따라 offset 계산
-
     const list = await this.PostService.getListPost(offset, limit);
 
-    console.log(list);
-    return { page: 'boardList.ejs', title: 'boardTitle', data: list };
+    return { page: 'board/list.ejs', title: 'boardTitle', data: list };
+  }
+
+  @Get('post/:post_id')
+  @Render('_admin/_layout/layout')
+  async post( @Param('post_id') post_id: number) {
+    // TODO: 비로그인시 admin 접근 로그인
+    // TODO: 관리자 아닐때 user 메인으로
+    const post = await this.PostService.getPost(post_id);
+    return { page: 'board/view.ejs', title: 'boardTitle', data: post };
   }
 }
